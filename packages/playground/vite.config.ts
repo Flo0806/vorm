326efa9b96ce6c5e78bfca0efdee993de11b774e
Florian Heuberger <fh@flogersoft.de>
2025-10-22 21:16:59 +0200

    added changeset config for next minor

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      "vorm-vue": path.resolve(__dirname, "../vorm/src"),
+      "vorm-vue/components": path.resolve(__dirname, "../vorm/src/components"),
+      // Legacy aliases (if needed)
       vorm: path.resolve(__dirname, "../vorm/src"),
       "vorm/components": path.resolve(__dirname, "../vorm/src/components"),
     },
